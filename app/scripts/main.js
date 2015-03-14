15b099f74f0a9919085e04bff017fb0fdcef2407
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-03-14 19:08:45 +0100

    Setup angular and add header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,23 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+var app = angular.module('app', ['ngRoute']);
+
+app.config(["$routeProvider", function ($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'partials/index.html',
+      controller: 'HomeController'
+    });
+}]);
+
+app.controller('HomeController', ['$scope', function ($scope) {
+  $scope.charityOrganizations = [
+    {
+      name: "Children's Cancer Foundation",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus leo, sodales placerat lobortis ut, tincidunt volutpat nisl. Sed nisl metus, blandit sed auctor eu, faucibus at neque.'
+    },
+    {
+      name: "Arms Wide Open Foundation",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus leo, sodales placerat lobortis ut, tincidunt volutpat nisl. Sed nisl metus, blandit sed auctor eu, faucibus at neque.'
+    }
+  ]
+}]);
