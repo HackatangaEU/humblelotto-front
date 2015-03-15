6c8cac71b832cd1f1ff033dfdae13820f7f44a74
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-03-15 10:01:49 +0100

    I don't remember anymore :poop:

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,4 @@
+"use strict";
 /* jshint devel:true */
 var app = angular.module('app', ['ngRoute']);
 
@@ -7,20 +8,20 @@ app.filter('moment', function () {
         momentObj = moment(input);
     return momentObj[momentFn].apply(momentObj, args);
   };
-})
+});
 
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider
     .when('/buy_ticket', {
-      templateUrl: 'partials/buy_ticket.html',
+      templateUrl: './partials/buy_ticket.html',
       controller: 'TicketsController'
     })
     .when('/organizations/:id', {
-      templateUrl: 'partials/organization.html',
+      templateUrl: './partials/organization.html',
       controller: 'OrganizationsController'
     })
     .otherwise({
-      templateUrl: 'partials/index.html',
+      templateUrl: './partials/index.html',
       controller: 'HomeController'
     });
 }]);
@@ -34,7 +35,7 @@ app.factory("OrganizationsFac", function ($http) {
 
   dataFactory.getOrganization = function (id) {
     return $http.get('http://pma.etiene.net/humblelotto/?r=organization/show&id=' + id);
-  }
+  };
 
   return dataFactory;
 });
@@ -55,24 +56,45 @@ app.controller('HomeController', ['$scope', 'OrganizationsFac', 'Lotto', functio
 
 }]);
 
-app.controller('TicketsController', ['$scope', function ($scope) {
+app.controller('TicketsController', ['$scope', 'OrganizationsFac', '$http', function ($scope, organizationsFac, $http) {
+
+  organizationsFac.getOrganizations().success(function (data) {
+    $scope.organizations = data;
+    $scope.organizationId = $scope.organizations[0].id;
+  });
+
   $scope.total = 0;
-  $scope.quantity = 0;
+
   $scope.value = 2.5;
+
   $scope.refreshTotal= function () {
     $scope.total = $scope.value * $scope.quantity;
-  }
-  $scope.organizations = [
-    {
-      name: "Children's Cancer Foundation",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus leo, sodales placerat lobortis ut, tincidunt volutpat nisl. Sed nisl metus, blandit sed auctor eu, faucibus at neque.'
-    },
-    {
-      name: "Arms Wide Open Foundation",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus leo, sodales placerat lobortis ut, tincidunt volutpat nisl. Sed nisl metus, blandit sed auctor eu, faucibus at neque.'
-    }
-  ]
+  };
+
+  $scope.submitRaffles = function () {
+    var raffle = {
+      address: $scope.walletAddress,
+      ntickets: $scope.quantity,
+      organization: $scope.organizationId
+    };
+    var url = 'http://pma.etiene.net/humblelotto/?r=payment/pay&address='
+    +raffle.address+"&ntickets="
+    +raffle.ntickets+"&organization="
+    +raffle.organization;
+    window.location = url;
+    // $http.get(url).success(function (data, status) {
+    //   console.log(data);
+    //   console.log(status);
+    // });
+  };
+
+  $scope.change = function (id) {
+    $scope.organizationId = id;
+    console.log($scope.organizationId);
+  };
+
 }]);
+
 
 app.controller('OrganizationsController', ['$scope', '$routeParams', 'OrganizationsFac', function ($scope, $routeParams, organizationFactory) {
    organizationFactory.getOrganization($routeParams.id).success(function (data) {
@@ -82,13 +104,13 @@ app.controller('OrganizationsController', ['$scope', '$routeParams', 'Organizati
 }]);
 
 app.factory("Lotto", function ($http) {
-  return $http.get('http://pma.etiene.net/humblelotto/?r=draw/getCurrent')
-})
+  return $http.get('http://pma.etiene.net/humblelotto/?r=draw/getCurrent');
+});
 
 $(document).ready(function () {
   $(".menu-ico").on("click", function () {
     $(".header-nav").toggle({
       duration: 300
     });
-  })
+  });
 });
