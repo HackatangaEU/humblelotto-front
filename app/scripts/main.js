4e91631ab9209da384fd3c4e02537e4ab8553c2c
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-03-15 07:05:44 +0100

    Fetch data from the sailor app for home and organization pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,21 @@
 /* jshint devel:true */
 var app = angular.module('app', ['ngRoute']);
 
+app.filter('moment', function () {
+  return function (input, momentFn /*, param1, param2, etc... */) {
+    var args = Array.prototype.slice.call(arguments, 2),
+        momentObj = moment(input);
+    return momentObj[momentFn].apply(momentObj, args);
+  };
+})
+
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider
     .when('/buy_ticket', {
       templateUrl: 'partials/buy_ticket.html',
       controller: 'TicketsController'
     })
-    .when('/organizations/:organizationId', {
+    .when('/organizations/:id', {
       templateUrl: 'partials/organization.html',
       controller: 'OrganizationsController'
     })
@@ -17,19 +25,34 @@ app.config(["$routeProvider", function ($routeProvider) {
     });
 }]);
 
-app.controller('HomeController', ['$scope', function ($scope) {
-  $scope.organizations = [
-    {
-      id: 1,
-      name: "Children's Cancer Foundation",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus leo, sodales placerat lobortis ut, tincidunt volutpat nisl. Sed nisl metus, blandit sed auctor eu, faucibus at neque.'
-    },
-    {
-      id: 2,
-      name: "Arms Wide Open Foundation",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus leo, sodales placerat lobortis ut, tincidunt volutpat nisl. Sed nisl metus, blandit sed auctor eu, faucibus at neque.'
-    }
-  ]
+app.factory("OrganizationsFac", function ($http) {
+  var dataFactory = {};
+
+  dataFactory.getOrganizations = function () {
+    return $http.get('http://pma.etiene.net/humblelotto/?r=organization/getList');
+  };
+
+  dataFactory.getOrganization = function (id) {
+    return $http.get('http://pma.etiene.net/humblelotto/?r=organization/show&id=' + id);
+  }
+
+  return dataFactory;
+});
+
+app.controller('HomeController', ['$scope', 'OrganizationsFac', 'Lotto', function ($scope, Organizations, Lotto) {
+  $scope.lotto = {
+    sum: 500
+  };
+
+  Organizations.getOrganizations().success(function (data) {
+    $scope.organizations = data;
+  });
+
+  Lotto.success(function (data) {
+    $scope.lotto = data;
+    console.log(data);
+  });
+
 }]);
 
 app.controller('TicketsController', ['$scope', function ($scope) {
@@ -51,12 +74,16 @@ app.controller('TicketsController', ['$scope', function ($scope) {
   ]
 }]);
 
-app.controller('OrganizationsController', ['$scope', function ($scope) {
-  $scope.organization = {
-    name: "Children's Cancer Foundation",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo enim, commodo eu turpis id, tincidunt porta elit. Proin quis mi in nisi tincidunt egestas sit amet nec libero. Etiam fringilla gravida purus sit amet vulputate. Aenean vitae neque ac ex iaculis vestibulum sed at augue. Quisque nec lacinia felis. Vestibulum at nibh eu enim malesuada molestie. Nulla gravida elementum lorem, id suscipit quam bibendum sed. Fusce nec risus nec lectus commodo dignissim eget a lectus. Aenean molestie, arcu vitae fermentum dictum, ante lacus malesuada ante, ut molestie erat tellus eget purus. </br>Morbi maximus dolor turpis, et tristique purus efficitur ut. Donec sed justo dignissim, eleifend leo nec, accumsan diam. Etiam facilisis, dolor id sollicitudin auctor, nibh purus consectetur nulla, vel tincidunt ligula augue eget ipsum. Maecenas sollicitudin quis ipsum ut bibendum. Phasellus id tempus nulla. Maecenas interdum cursus augue, id consectetur sem posuere sit amet.Sed ex tortor, tempus vitae risus fringilla, dignissim dignissim lectus. Curabitur eu dui erat. Suspendisse iaculis massa pharetra sapien varius fermentum. Aenean accumsan risus nec lacinia cursus. Vestibulum varius, velit eget blandit bibendum, libero ante pulvinar erat, nec lobortis libero lorem sed mi. Duis eu justo tincidunt, tincidunt turpis nec, iaculis turpis. Aenean placerat tempor gravida. Sed turpis lectus, mollis at nisi a, faucibus imperdiet dui. In egestas ipsum a ante bibendum, at sodales lorem lobortis. In semper dolor lorem, imperdiet dignissim lacus mattis id. Quisque sodales nibh eu elementum pellentesque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer vehicula mauris quis justo consectetur suscipit."
-  }
+app.controller('OrganizationsController', ['$scope', '$routeParams', 'OrganizationsFac', function ($scope, $routeParams, organizationFactory) {
+   organizationFactory.getOrganization($routeParams.id).success(function (data) {
+     $scope.organization = data;
+     console.log(data);
+   });
 }]);
+
+app.factory("Lotto", function ($http) {
+  return $http.get('http://pma.etiene.net/humblelotto/?r=draw/getCurrent')
+})
 
 $(document).ready(function () {
   $(".menu-ico").on("click", function () {
